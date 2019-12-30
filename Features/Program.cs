10b57d96c025dcffb6728967504da71fe4515b39
Outmane Baladi <outmanebaladi22@gmail.com>
2019-12-30 16:19:31 +0100

    The power of IEnumerable

---
+++ Features/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            { 
+                new Employee() { Id = 1, Name = "Outmane" }, 
+                new Employee() { Id = 2, Name = "Hamza" }
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee() { Id = 3, Name = "Mehdi"}
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+        }
+    }
+}
